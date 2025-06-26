dcf1ba2651b3027dd9a54913d792f23a16a98180
Daniel Hernández <dannhc1234@gmail.com>
2025-06-26 16:13:00 -0600

    Diseño perfil, ya agrega especialistas

--- Controlador/UsuariasControlador.php
+++ Controlador/UsuariasControlador.php
@@ -4,10 +4,9 @@ include("../modelo/Usuarias.php");
 
 $u = new Usuarias();
 $u->conectarBD();
+
 switch ($_REQUEST['opcion']) {
     case 1:
-        $documento = isset($_FILES['documento']) && $_FILES['documento']['error'] == 0 ? $_FILES['documento'] : null;
-
         $u->inicializar(
             $_REQUEST['nombre'],
             $_REQUEST['apellidos'],
@@ -16,8 +15,7 @@ switch ($_REQUEST['opcion']) {
             $_REQUEST['contraseña'],
             $_REQUEST['conContraseña'],
             $_REQUEST['fecha_nac'],
-            $_REQUEST['rol'],
-            $documento
+            $_REQUEST['rol']
         );
         $u->agregarUsuaria();
         break;
