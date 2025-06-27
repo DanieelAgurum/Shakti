344a32d5dd8233c6735e25d3e74a945ecc711971
Daniel Hernández <dannhc1234@gmail.com>
2025-06-26 18:47:52 -0600

    perfil con modales

--- Vista/profileEspecialista.php
+++ Vista/profileEspecialista.php
@@ -1,3 +1,9 @@
+<?php
+if (empty($_SESSION['correo']) and empty($_SESSION['id_rol']) === 2) {
+    header("Location: ../../index.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -12,6 +18,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     <?php include '../components/usuaria/navbar.php'; ?>
+    <?php include 'modales/perfil.php'; ?>
 </head>
 
 <body>
@@ -28,48 +35,10 @@
                                     <h4>John Doe</h4>
                                     <p class="text-secondary mb-1">Full Stack Developer</p>
                                     <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                                    <button class="btn btn-outline-primary">Message</button>
+                                    <button class="btn btn-outline-primary">Mensaje</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card mt-3">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline">
-                                        <circle cx="12" cy="12" r="10"></circle>
-                                        <line x1="2" y1="12" x2="22" y2="12"></line>
-                                        <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-                                    </svg>Website</h6>
-                                <span class="text-secondary">https://bootdey.com</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline">
-                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                    </svg>Github</h6>
-                                <span class="text-secondary">bootdey</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info">
-                                        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                                    </svg>Twitter</h6>
-                                <span class="text-secondary">@bootdey</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger">
-                                        <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                                    </svg>Instagram</h6>
-                                <span class="text-secondary">bootdey</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary">
-                                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                                    </svg>Facebook</h6>
-                                <span class="text-secondary">bootdey</span>
-                            </li>
-                        </ul>
                     </div>
                 </div>
                 <div class="col-md-8">
@@ -120,12 +89,14 @@
                                 </div>
                             </div>
                             <hr>
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addNew" style="margin-bottom: 8px;">
-                                        <i class="fa-solid fa-circle-plus"></i> Editar perfil
-                                    </button>
-                                </div>
+                            <div class="d-flex justify-content-end gap-2 mb-3">
+                                <button type="button" class="btn btn-primary position-relative" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    Completar perfil
+                                    <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+                                </button>
+                                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addNew">
+                                    <i class="fa-solid fa-circle-plus"></i> Editar perfil
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -191,73 +162,8 @@
         </div>
     </div>
 
-    <div class="modal fade" id="addNew" tabindex="-1" aria-labelledby="addNewLabel" aria-hidden="true">
-        <div class="modal-dialog w-125" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title text-center" id="addNewLabel"><strong>Editar perfil</strong></h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <form method="post" action="../../control/productoCtrl.php" enctype="multipart/form-data">
-                            <div class="row form-group">
-                                <div class="col-sm-2">
-                                    <label class="control-label">Foto:</label>
-                                </div>
-                                <div class="col-sm-10">
-                                    <input type="file" class="form-control" name="imagenProduc" required>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-                                <div class="col-sm-2">
-                                    <label class="control-label">Nombre:</label>
-                                </div>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="nombreProduc" required>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-                                <div class="col-sm-2">
-                                    <label class="control-label">Categoría:</label>
-                                </div>
-                                <div class="col-sm-10">
-                                    <select name="categoriaProduc" class="form-select" required>
-                                        <!-- Opciones aquí -->
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-                                <div class="col-sm-2">
-                                    <label class="control-label">Precio:</label>
-                                </div>
-                                <div class="col-sm-10">
-                                    <input type="number" class="form-control" name="precioProduc" required>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-                                <div class="col-sm-2">
-                                    <label class="control-label">Stock:</label>
-                                </div>
-                                <div class="col-sm-10">
-                                    <input type="number" class="form-control" name="stockProduc" required>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                                    <i class="fa-solid fa-ban"></i> Cancelar
-                                </button>
-                                <input type="hidden" value="1" name="opcion">
-                                <button type="submit" name="add" value="addNew" class="btn btn-primary">
-                                    <i class="fa-solid fa-circle-check"></i> Guardar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
