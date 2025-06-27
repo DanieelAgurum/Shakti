240de14adfa3a16553330435e0a4c08e9193232a
CristoferAnthony089 <cristo045millanperez@gmil.com>
2025-06-27 11:30:08 -0600

    Cambios de IS

--- Vista/login.php
+++ Vista/login.php
@@ -29,7 +29,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <!-- Bootstrap Icons -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+  <script src="../peticiones(js)/iniciarSesion.js"></script>
   <style>
     :root {
       --morado-base: #5a2a83;
@@ -129,8 +129,6 @@ if (session_status() === PHP_SESSION_NONE) {
 
   <!-- Bootstrap JS Bundle -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-  <script src="../peticiones(js)/iniciarSesion.js"></script>
 
 </body>
 
