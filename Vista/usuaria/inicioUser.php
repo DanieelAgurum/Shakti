328e3faab3c6671b1359ca74f9143f301b98c75a
Brian Yahir <252271004@alumnos.utn.edu.mx>
2025-06-30 03:37:43 -0500

    ---SPRINT 1 END---

--- Vista/usuaria/inicioUser.php
+++ Vista/usuaria/inicioUser.php
@@ -1,19 +1,7 @@
-<div class="container mt-5 text-center">
+<div class="container text-center">
   <h1>¡Hola, <?= htmlspecialchars($_SESSION['nickname']) ?>!</h1>
   <p class="lead">Nos alegra tenerte aquí. Accede a nuestros recursos y herramientas pensadas para tu bienestar.</p>
 
-  <div class="row mt-4">
-    <div class="col-md-6">
-      <h3>Mi perfil</h3>
-      <p>Consulta o edita tu información personal.</p>
-      <a href="<?= $urlBase ?>views/usuaria/perfil.php" class="btn btn-primary">Ir al perfil</a>
-    </div>
-    <div class="col-md-6">
-      <h3>Recursos disponibles</h3>
-      <p>Consulta artículos, herramientas y más contenido exclusivo.</p>
-      <a href="#" class="btn btn-outline-secondary">Ver recursos</a>
-    </div>
-  </div>
 
   <hr class="my-5">
 </div>
