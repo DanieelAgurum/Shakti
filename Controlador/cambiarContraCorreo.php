8adf520030c8c2e87b24f7de831ce50cdddda0d3
CristoferAnthony089 <cristo045millanperez@gmil.com>
2025-06-30 20:59:46 -0600

    enviar correo

---
+++ Controlador/cambiarContraCorreo.php
@@ -0,0 +1,26 @@
+<?php
+include_once '../Modelo/cambiarContraCorreo.php';
+include_once '../obtenerLink/obtenerLink.php';
+
+$token = new cambiarContraCorreo();
+$urlBase = getBaseUrl();
+
+if (isset($_GET['opcion'])) {
+    $opcion = $_GET['opcion'];
+
+    switch ($opcion) {
+        case 1:
+            if (isset($_REQUEST['correo'])) {
+                $token->inicializar($_REQUEST['correo'], $urlBase);
+                $token->enviarToken();
+            } else {
+                header("Location: {$urlBase}/index.php");
+                exit;
+            }
+            break;
+
+        default:
+            header("Location: {$urlBase}/index.php");
+            exit;
+    }
+}
