a390432778b1571326fc066c5880cbfcd5b22a7f
CristoferAnthony089 <cristo045millanperez@gmil.com>
2025-07-08 02:50:00 -0600

    reportes para post de la usuaria

--- Vista/usuaria/foro.php
+++ Vista/usuaria/foro.php
@@ -14,18 +14,20 @@ $urlBase = getBaseUrl();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Foro - Shakti</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="<?= $urlBase ?>css/estilos.css" />
     <link rel="stylesheet" href="<?= $urlBase ?>css/estiloscarrucel.css" />
     <link rel="stylesheet" href="<?= $urlBase ?>css/publicaciones.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <script src="<?= $urlBase ?>validacionRegistro/abrirComentarios.js"></script>
-    <script src="<?= $urlBase ?>peticiones(js)/buscadorEnForo.js"></script>
+    <script src="<?= $urlBase ?>peticiones(js)/mandarReporte.js"></script>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/Shakti/components/usuaria/navbar.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Shakti/Vista/modales/reportarPostUsuarias.php'; ?>
 
     <style>
         .dropdown {
