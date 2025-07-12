ace0f24fc75c736e78378ac41c01f89e68601f4d
Daniel Hernández <dannhc1234@gmail.com>
2025-07-12 10:36:47 -0600

    notas

--- Vista/usuaria/perfil.php
+++ Vista/usuaria/perfil.php
@@ -1,5 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Shakti/obtenerLink/obtenerLink.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Shakti/Modelo/notasModelo.php';
 $urlBase = getBaseUrl();
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -24,6 +25,8 @@ if (!empty($_SESSION['foto'])) {
     $fotoSrc = 'https://cdn1.iconfinder.com/data/icons/avatar-3/512/Secretary-512.png';
 }
 
+$notasModel = new Notas();
+$notas = $notasModel->obtenerNotas()
 ?>
 
 
@@ -44,25 +47,58 @@ if (!empty($_SESSION['foto'])) {
     <div class="container mt-5">
         <div class="main-body">
             <div class="row gutters-sm">
+                <!-- Columna izquierda con la foto y botones -->
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center text-center">
-                                <img src="<?php echo $fotoSrc; ?>" alt="<?php echo isset($_SESSION['nombre_rol']) ? ucwords(strtolower($_SESSION['nombre_rol'])) : " " ?>" class="rounded-circle" width="150" height="150" width="150">
+                                <img src="<?php echo $fotoSrc; ?>" alt="<?php echo isset($_SESSION['nombre_rol']) ? ucwords(strtolower($_SESSION['nombre_rol'])) : " " ?>" class="rounded-circle" width="150" height="150">
                                 <div class="mt-3">
                                     <h4><?php echo isset($_SESSION['nombre']) ? ucwords(strtolower($_SESSION['nombre'])) : " " ?></h4>
                                     <p class="text-secondary mb-1"><?php echo isset($_SESSION['descripcion']) ? ucwords(strtolower($_SESSION['descripcion'])) : " " ?></p>
-                                    <button class="btn btn-outline-primary"><i class="bi bi-envelope-paper-heart-fill"></i> Mensajes</button>
-                                    <button class="btn btn-outline-secondary"><i class="bi bi-book-fill"></i> Diario</button>
+                                    <button class="btn btn-outline-primary">
+                                        <i class="bi bi-envelope-paper-heart-fill"></i> Mensajes
+                                    </button>
+                                    <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">
+                                        <i class="bi bi-book-fill"></i> Notas
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <!-- Notas -->
+                    <div class="card mt-3">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">Notas recientes</h6>
+                            </li>
 
+                            <?php foreach ($notas as $index => $nota): ?>
+                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                    <h6 class="mb-0">
+                                        <i class="bi bi-journal"></i> <?= htmlspecialchars($nota['titulo']) ?>
+                                    </h6>
+                                    <span class="text-secondary">
+                                        <a href="#" data-bs-toggle="modal" data-bs-target="#notaModal<?= $index ?>">
+                                            <i class="bi bi-eye"></i>
+                                        </a>
+                                    </span>
+                                </li>
+                                <?php include '../modales/notas.php'; ?>
+                            <?php endforeach; ?>
+                            <li class="list-group-item d-flex justify-content-end align-items-center">
+                                <h6 class="mb-0">
+                                    <a href="" class="text-decoration-none">Ver más</a>
+                                </h6>
+                            </li>
+                        </ul>
+                    </div>
+                </div> <!-- /col-md-4 -->
+                <!-- Columna derecha con datos del perfil -->
                 <div class="col-md-8">
                     <div class="card mb-3">
                         <div class="card-body">
+
                             <div class="row">
                                 <div class="col-sm-3">
                                     <h6 class="mb-0">Nombre completo</h6>
@@ -74,12 +110,11 @@ if (!empty($_SESSION['foto'])) {
                                         : " ";
                                     ?>
                                 </div>
-
                             </div>
                             <hr>
                             <div class="row">
                                 <div class="col-sm-3">
-                                    <h6 class="mb-0">Correo eléctronico</h6>
+                                    <h6 class="mb-0">Correo electrónico</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
                                     <?php echo isset($_SESSION['correo']) ? $_SESSION['correo'] : " "; ?>
@@ -109,7 +144,7 @@ if (!empty($_SESSION['foto'])) {
                                     <h6 class="mb-0">Nombre de usuaria</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <?php echo isset($_SESSION['nickname']) ? $_SESSION['nickname'] : ""  ?>
+                                    <?php echo isset($_SESSION['nickname']) ? $_SESSION['nickname'] : "" ?>
                                 </div>
                             </div>
                             <hr>
@@ -120,10 +155,13 @@ if (!empty($_SESSION['foto'])) {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                </div> <!-- /col-md-8 -->
+            </div> <!-- /row -->
+        </div> <!-- /main-body -->
+    </div> <!-- /container -->
+
+    <?php include '../modales/notas.php'; ?>
+
     <?php if (isset($_GET['status']) && isset($_GET['message'])): ?>
         <script>
             Swal.fire({
@@ -134,6 +172,8 @@ if (!empty($_SESSION['foto'])) {
             });
         </script>
     <?php endif; ?>
+
+    <script src="../../validacionRegistro/notas.js"></script>
     <script src="<?= $urlBase ?>peticiones(js)/mandarMetricas.js.php?vista=<?= urlencode(basename($_SERVER['PHP_SELF'])) ?>"></script>
     <?php include '../modales/perfil.php'; ?>
     <script src="../../validacionRegistro/validacionActualizacion.js"></script>
