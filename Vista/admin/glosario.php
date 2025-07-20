c914fb1cba58a642850ebf8f473e3628bbc83f6b
CristoferAnthony089 <127165024+CristoferAnthony089@users.noreply.github.com>
2025-07-19 18:17:17 -0600

    pinche glosario, voy a la mitad ya me empute con el

--- Vista/admin/glosario.php
+++ Vista/admin/glosario.php
@@ -14,10 +14,14 @@ if (empty($_SESSION['correo']) || $_SESSION['id_rol'] != 3) {
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tipo de Reportes - Shakti</title>
-    <script src="js/tiposReportes.js"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Glosario - Shakti</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-pbciWgAl4KuOkZ5vGQZzELtiQ+3I6Mxn9c8E9p8ywRrbKwHmeEpeL6PH20FqSb70vPjJOeTEJjFZ32Vb9I55Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/eacdd605ec.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.tiny.cloud/1/bnw4wazqztadr4il1bfgdflo063pyw0wvki8x1q8d9xx0akz/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="js/tinyMC.js"></script>
+    <script src="js/glosario.js"></script>
 </head>
 
 <body class="sb-nav-fixed">
@@ -26,16 +30,51 @@ if (empty($_SESSION['correo']) || $_SESSION['id_rol'] != 3) {
     <div id="layoutSidenav">
         <?php
         include $_SERVER['DOCUMENT_ROOT'] . '/Shakti/components/admin/lateral.php';
-        include $_SERVER['DOCUMENT_ROOT'] . '/Shakti/Vista/admin/modales/tipo_reporte.php';
+        include $_SERVER['DOCUMENT_ROOT'] . '/Shakti/Vista/admin/modales/glosario.php';
         ?>
 
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4 mb-5">
                     <div class="container">
-                        <h1 class="page-header text-center"><strong>Tipo de Reportes</strong></h1>
+                        <h1 class="page-header text-center"><strong>Glosario</strong></h1>
+                        <button
+                            type="button"
+                            style="margin-bottom: 8px;"
+                            class="btn btn-primary"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">
+                            Nuevo <i class="fa-solid fa-circle-plus"></i>
+                        </button>
                         <div class="row">
                             <div class="col-sm-12">
-                                <button type="button" style="margin-bottom: 8px;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Nuevo <i class="fa-solid fa-circle-plus"></i>
-                                </button>
+                                <table class="table table-bordered table-striped" id="MiAgenda" style="margin-top:20px center;">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Icono</th>
+                                            <th>Titulo</th>
+                                            <th>Descripción</th>
+                                            <th>Opciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        require_once $_SERVER['DOCUMENT_ROOT'] . '/Shakti/Modelo/GlosarioMdl.php';
+                                        $glo = new GlosarioMdl();
+                                        $glo->verGlosario();
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
+<script src="js/tabla.js"></script>
+
+</html>
