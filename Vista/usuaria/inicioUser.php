84ace9a7b509be308056fb9cc5b51c5ec79091a1
CristoferAnthony089 <127165024+CristoferAnthony089@users.noreply.github.com>
2025-07-26 00:55:14 -0600

    correciones menores

--- Vista/usuaria/inicioUser.php
+++ Vista/usuaria/inicioUser.php
@@ -1,7 +1,9 @@
 <div class="container text-center">
   <h1>¡Hola, <?= htmlspecialchars($_SESSION['nickname']) ?>!</h1>
   <p class="lead">Nos alegra tenerte aquí. Accede a nuestros recursos y herramientas pensadas para tu bienestar.</p>
-
-
+  <div class="hero-buttons ">
+    <a href="#" class="btn btn-primary me-2">Más contenido....</a>
+    <a href="<?= $urlBase ?>Vista/organizacionVista.php" class="btn btn-outline-secondary">Organizaciones</a>
+  </div>
   <hr class="my-5">
 </div>
