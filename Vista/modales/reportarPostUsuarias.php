d669f571e062f151e670c98c386e57747d7c6d40
CristoferAnthony089 <127165024+CristoferAnthony089@users.noreply.github.com>
2025-07-30 00:50:43 -0600

    corrección de disño en el modal de compartir

--- Vista/modales/reportarPostUsuarias.php
+++ Vista/modales/reportarPostUsuarias.php
@@ -21,7 +21,7 @@ $consulta = $conn->query($sql);
                     <input type="hidden" id="id_usuaria" name="id_usuaria" value="<?= $_SESSION['id_usuaria'] ?>">
                     <input type="hidden" name="opcion" value="1">
                     <input type="hidden" id="publicacion" name="publicacion">
-                    <input type="hidden" name="tipo_de_reporte" value="3">
+                    <input type="hidden" name="tipo_de_reporte" value="">
                     <?php foreach ($consulta as $registro): ?>
                         <div class="form-check form-check-inline">
                             <label class="form-check-label" for="reporte<?= $registro['id_tipo_reporte'] ?>">
@@ -43,24 +43,26 @@ $consulta = $conn->query($sql);
 <div class="modal fade" id="modalCompartir" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Compartir</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-header bg-info position-relative">
+                <h1 class="modal-title fs-5 position-absolute top-50 start-50 translate-middle" id="exampleModalLabel">
+                    Compartir
+                </h1>
+                <button type="button" class="btn btn-close btn-sm ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-center">
                 <div class="d-flex justify-content-center gap-3 flex-wrap">
-                    <a href="#" onclick="compartirWhatsapp()" class="btn btn-success" title="Compartir por WhatsApp">
+                    <a href="#" onclick="compartirWhatsapp()" class="btn btn-outline-success" title="Compartir por WhatsApp">
                         <i class="bi bi-whatsapp"></i> WhatsApp
                     </a>
-                    <a href="#" onclick="compartirFacebook()" class="btn btn-primary" title="Compartir en Facebook">
+                    <a href="#" onclick="compartirFacebook()" class="btn btn-outline-primary" title="Compartir en Facebook">
                         <i class="bi bi-facebook"></i> Facebook
                     </a>
-                    <a href="#" onclick="compartirTwitter()" class="btn btn-info text-white" title="Compartir en X (Twitter)">
-                        <i class="bi bi-twitter-x"></i> X
+                    <a href="#" onclick="compartirTwitter()" class="btn btn-outline-dark" title="Compartir en X (Twitter)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                        </svg>
                     </a>
                 </div>
-            </div>
-            <div class="modal-footer">
             </div>
         </div>
     </div>
