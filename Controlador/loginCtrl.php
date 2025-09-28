3a64ef2507f2d04ca26c3b89d4f3086da236ca15
Brian Yahir <252271004@alumnos.utn.edu.mx>
2025-09-27 22:42:36 -0500

    inicio 1.1

--- Controlador/loginCtrl.php
+++ Controlador/loginCtrl.php
@@ -1,18 +1,18 @@
 <?php
-
 include($_SERVER['DOCUMENT_ROOT'] . "/shakti/Modelo/loginMdl.php");
 
 $u = new loginMdln();
-$u -> conectarBD();
 
-switch($_REQUEST['opcion']){
+switch($_POST['opcion'] ?? ''){
     case 1:
-        $u->inicializar($_REQUEST['correo'], $_REQUEST['contraseña']);
-        $u->iniciarsesion();
+        $u->inicializar($_POST['correo'], $_POST['contraseña']);
+        $u->iniciarSesion(); // ojo: inicia sesión y redirige
         break;
     case 2:
         $u->cerrarSesion();
         break;
-    }
-
+    default:
+        header("Location: ../Vista/login.php");
+        exit;
+}
 ?>
