ef26336c8aa5251b8358d12cfac4045bfe279248
Daniel Hernández <dannhc1234@gmail.com>
2025-09-29 02:03:24 -0600

    ñ

--- peticiones(js)/return.js
+++ peticiones(js)/return.js
@@ -1,13 +1,13 @@
-const btnTop = document.getElementById("btn-top");
+const shaktiBtnTop = document.getElementById("shakti-btn-top");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
-        btnTop.classList.add("show");
+        shaktiBtnTop.classList.add("show");
     } else {
-        btnTop.classList.remove("show");
+        shaktiBtnTop.classList.remove("show");
     }
 });
 
-btnTop.addEventListener("click", () => {
+shaktiBtnTop.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
