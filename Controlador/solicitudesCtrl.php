139aa227cbd3c6c3eb06aeb6f017f166db13f22b
CristoferAnthony089 <127165024+CristoferAnthony089@users.noreply.github.com>
2025-10-18 13:08:03 -0600

    mejoras del chat

--- Controlador/solicitudesCtrl.php
+++ Controlador/solicitudesCtrl.php
@@ -6,42 +6,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/shakti/Modelo/solicitudesMdl.php';
 
 $soli = new SolicitudesMdl();
 
-if (isset($_GET['solicitudes'])) {
-    $soli->obtenerSolicitudes();
-    return;
-}
-
-if (isset($_GET['usuarios'])) {
-    $soli->obtenerUsuarios();
-    return;
-}
-
-if (isset($_GET['agregarAmigo'])) {
-    $soli->inicializar($_REQUEST['nickname']);
-    $soli->enviarSolicitud();
-    return;
-}
-
-if (isset($_GET['cancelarSolicitud'])) {
-    $soli->cancelarSolicitud($_REQUEST['nickname']);
-    return;
-}
-
-if (isset($_GET['aceptarSolicitud'])) {
-    $soli->aceptarSolicitud($_REQUEST['nickname']);
-    return;
-}
-
-if (isset($_GET['rechazarAmigo'])) {
-    $soli->rechazarSolicitud($_REQUEST['nickname']);
-    return;
-}
-
-
-if (isset($_GET['eliminarAmigo'])) {
-    $soli->eliminarAmigo($_REQUEST['nickname']);
-    return;
-}
 
 if (isset($_GET['especialistas'])) {
     $limit = isset($_GET['limit']) ? intval($_GET['limit']) : 10;
