f1cf8c7b48509fb10fa78ac6587927659797ca2e
Daniel Hernández <dannhc1234@gmail.com>
2025-11-06 18:24:53 -0600

    logo se ve mrd

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == 3) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Shakti</title>
+  <title>NexoH</title>
   <!-- Librerías adicionales en el head del navbar -->
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
