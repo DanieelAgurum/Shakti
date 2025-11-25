4a8e2636179d5c2cf0221af2c628395e7e004cd0
Brian Yahir <252271004@alumnos.utn.edu.mx>
2025-11-24 23:40:55 -0600

    loginGoogleProduccion

--- Modelo/configuracionG.php
+++ Modelo/configuracionG.php
@@ -1,8 +1,16 @@
 <?php
 $clientID     = "149987150313-s4k1pr3ggbi12phgsfspsbf6mnukocc8.apps.googleusercontent.com";
 $clientSecret = "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";
-$redirectUri  = "http://localhost/Shakti/Vista/login.php";
-$redirectUri  = "http://localhost/Shakti/Vista/loginGoogle.php";  
+
+
+$dominio = $_SERVER['HTTP_HOST'];
+
+if ($dominio === "localhost") {
+    $redirectUri = "http://localhost/Shakti/Vista/loginGoogle.php";
+} else {
+    // PRODUCCIÓN
+    $redirectUri = "https://shaktiapp.site/Vista/loginGoogle.php";
+}
 
 $client = new Google_Client();
 $client->setClientId($clientID);        
